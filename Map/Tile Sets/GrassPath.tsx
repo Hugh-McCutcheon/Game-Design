a668e108d124a77ae6841fe8ef8946290077549c
Your Name <mccutcheonh@student.waihekehigh.school.nz>
2021-06-30 09:24:43 +1200

    The main changes where some sprite and map changes

--- Map/Tile Sets/GrassPath.tsx
+++ Map/Tile Sets/GrassPath.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Grass/Path" tilewidth="64" tileheight="64" tilecount="48" columns="4">
- <image source="../../../OneDrive/Documents/Pixel Art/Grass tile set.png" width="256" height="576"/>
+<tileset version="1.5" tiledversion="1.6.0" name="Grass/Path" tilewidth="32" tileheight="32" tilecount="1176" columns="56">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="../../../OneDrive/Documents/Pixel Art/Tilesets/1.3 grass.png" width="1792" height="672"/>
 </tileset>
